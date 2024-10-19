f48baa67d268b4acd9a9bd9c5952ec039174b08f
Sunit Mal <sunitmal1832002@gmail.com>
2024-10-19 23:56:35 +0530

    Admin Panel changes

--- testflow/admin.py
+++ testflow/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
-from .models import ResetPasswordRequest
+from .models import ResetPasswordRequest, Prompt, TestResult
 
 # Register your models here.
 
+
 @admin.register(ResetPasswordRequest)
 class ResetPasswordRequestAdmin(admin.ModelAdmin):
-	list_display = ['userEmail', 'request_at','updated_at', 'encryptCode', 'user','verified']
+    list_display = [
+        "userEmail",
+        "request_at",
+        "updated_at",
+        "encryptCode",
+        "user",
+        "verified",
+    ]
+
+
+@admin.register(Prompt)
+class PromptAdmin(admin.ModelAdmin):
+    list_display = ["user", "prompt_text", "created_at"]
+
+
+@admin.register(TestResult)
+class TestResultAdmin(admin.ModelAdmin):
+    list_display = [
+        "prompt",
+        "coverage",
+        "created_at",
+        "code",
+        "test_cases",
+        "test_output",
+        "passed_tests",
+        "failed_tests",
+    ]
